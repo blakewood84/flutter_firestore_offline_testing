ac1d37078786979404d8b2014abbc3129802eded
Blake Wood Jr <blakewoodjr84@gmail.com>
2023-09-22 12:19:11 -0700

    Add boilerplate for inspections.

---
+++ lib/home/view/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SizedBox(
+            width: constraints.maxWidth,
+            height: constraints.maxHeight,
+            child: Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Create New Inspection',
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/home/widgets/inspection_stream.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InspectionStream extends StatelessWidget {
+  const InspectionStream({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
