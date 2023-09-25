3426bace8d411c158b7e6559e896edc9bae4621f
Blake Wood Jr <blakewoodjr84@gmail.com>
2023-09-25 12:34:54 -0700

    Add Stream content.

--- lib/home/widgets/inspection_stream.dart
+++ lib/home/widgets/inspection_stream.dart
@@ -9,11 +9,39 @@ class InspectionStream extends StatelessWidget {
     return StreamBuilder(
       stream: FirebaseFirestore.instance.collection('inspections').snapshots(),
       builder: (context, snapshot) {
+        if (!snapshot.hasData || snapshot.hasError) {
+          return const SizedBox.shrink();
+        }
+
+        final docs = snapshot.data?.docs;
+        final length = docs?.length ?? 0;
+
+        if (docs == null || length == 0) {
+          return const SizedBox.shrink();
+        }
+
         return ListView.separated(
-          itemCount: 2,
+          itemCount: length,
           itemBuilder: (context, index) {
+            final doc = docs.elementAt(index);
+            final name = doc.get('name') as String? ?? 'Unknown';
+            final approved = doc.get('approved') as bool? ?? false;
+            final date = doc.get('date') as Timestamp?;
+
             return ListTile(
-              title: Text('Title'),
+              title: Text(name),
+              leading: approved
+                  ? const Icon(
+                      Icons.check_circle,
+                      color: Colors.green,
+                    )
+                  : const Icon(
+                      Icons.cancel,
+                      color: Colors.red,
+                    ),
+              subtitle: Text(
+                date?.toDate().toString() ?? '---',
+              ),
             );
           },
           separatorBuilder: (context, index) {
